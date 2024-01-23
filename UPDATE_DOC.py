4e9f746542a9e35eb8a9ce2bf1706a3569df7d6f
mursalatul <mursalatulislampallob@gmail.com>
2024-01-24 01:03:11 +0600

    reformat

--- UPDATE_DOC.py
+++ UPDATE_DOC.py
@@ -1,5 +1,60 @@
 import os, json
 
+
+class HandleJson:
+    # reformat data as actual json format
+    def add_link(self):
+        self.dic = {
+            # "folder_number" : f"{len(self.data)}",
+            # "folder1" : {
+            #     "folder1_url" : "",
+            #     "file_number" : "",
+            #     "file1" : "file1 url",
+            #     "file2" : "file2 url"
+            # },
+        }
+
+        for folder, file in self.data.items():
+            inner_dic = dict()
+            inner_dic["folder_url"] = "https://github.com/mursalatul/code-park/tree/master/" + folder
+            #inner_dic["file_number"] = f"{len(file)}"
+            # sort is working locally but is not working in github action. will fix it later
+            # file.sort()
+            for f in file:
+                inner_dic[f] = inner_dic["folder_url"] + f"/{f}"
+            self.dic[folder] = inner_dic
+        
+
+    def write_json(self, data):
+        self.data = data
+        self.add_link()
+        with open("DOC_DATA.json", "w") as outfile: 
+            json.dump(self.dic, outfile, indent=4)
+
+
+class CreateDocumentation:
+    def writedoc(self, folder_file: dict):
+        
+        with open("DOCUMENTATION.md", "w") as doc:
+            doc.write("# Code Park Documentation\n\
+This documentation provides a front view of the content presented in the repository. Here, you can see the folders and the files with a tree-view structure.\n\n")
+
+            doc.write("## Content\n")
+
+            # sorting the folders before adding to doc
+            folder_file = dict(sorted(folder_file.items()))
+
+            # getting folder name and file list
+            for folder, file in folder_file.items():
+                # writing folder name to doc
+                doc.write(f"- [{folder}](https://github.com/mursalatul/code-park/tree/master/{folder})\n")
+                # sorting the files for organized view in doc
+                #file.sort()
+                # adding the file names
+                for files in file:
+                    doc.write(f"  - [{files}](https://github.com/mursalatul/code-park/tree/master/{folder}/{files})\n")
+
+
 class Files:
     # this char will not accepted inside a class
     RESTRICTED_CHAR = [".", "-", "!", "@", "#", "$", "%", "^", "&", "*", 
@@ -114,3 +169,24 @@ class Files:
         else:
             return self.isgoodname(folder_name)
         
+
+
+def main():
+    # getting repository location
+    cwd = os.getcwd()
+
+    # getting file data
+    ff = Files()
+    data = (ff.get_all_valid_folder_files_dict(cwd))
+    print(data)
+
+    # writing to Documentation.md
+    doc = CreateDocumentation()
+    doc.writedoc(data) # documentation will be written in the repo home
+
+    # updating the json file
+    mjson = HandleJson()
+    mjson.write_json(data)
+    
+if __name__ == '__main__':
+    main()
